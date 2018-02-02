219a3bc0ff8a9795f8f6aa12c922c2c19df4507e
xur <xur@2345.com>
2018-02-02 16:58:07 +0800

    add request

--- app/src/main/java/xur/com/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/xur/com/weatherapp/ui/activities/MainActivity.kt
@@ -1,12 +1,16 @@
-package xur.com.weatherapp.activities
+package xur.com.weatherapp.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 import xur.com.weatherapp.R
 import xur.com.weatherapp.adapters.ForecastListAdapter
+import xur.com.weatherapp.data.Request
 
 class MainActivity : AppCompatActivity() {
     private val items = listOf(
@@ -25,5 +29,13 @@ class MainActivity : AppCompatActivity() {
         val forecastList = find<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
     }
 }
