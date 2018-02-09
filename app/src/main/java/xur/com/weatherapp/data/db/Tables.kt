76ccc1529e7d87ed7b21c5fb89e895e141b67a4e
xur <xur@2345.com>
2018-02-09 16:44:55 +0800

    add SQLiteOpenHelper

---
+++ app/src/main/java/xur/com/weatherapp/data/db/Tables.kt
@@ -0,0 +1,22 @@
+package xur.com.weatherapp.data.db
+
+/**
+ * Created by xur on 2018/2/9.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
