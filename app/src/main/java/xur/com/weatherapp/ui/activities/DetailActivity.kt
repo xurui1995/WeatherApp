2f0a0db68c4a588fa61a81e5070254b22a1bc340
xur <xur@2345.com>
2018-02-22 17:52:08 +0800

    finish detail

--- app/src/main/java/xur/com/weatherapp/ui/activities/DetailActivity.kt
+++ app/src/main/java/xur/com/weatherapp/ui/activities/DetailActivity.kt
@@ -2,7 +2,20 @@ package xur.com.weatherapp.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detail.*
+import org.jetbrains.anko.ctx
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import xur.com.weatherapp.R
+import xur.com.weatherapp.domain.commands.RequestDayForecastCommand
+import xur.com.weatherapp.domain.model.Forecast
+import xur.com.weatherapp.extensions.color
+import xur.com.weatherapp.extensions.textColor
+import xur.com.weatherapp.extensions.toDateString
+import java.text.DateFormat
+
 /**
  * Created by xur on 2018/2/19.
  */
@@ -17,6 +30,25 @@ class DetailActivity: AppCompatActivity() {
         setContentView(R.layout.activity_detail)
         title = intent.getStringExtra(CITY_NAME)
 
+        doAsync {
+            val result =  RequestDayForecastCommand(intent.getLongExtra(ID, -1)).execute()
+            uiThread { bindForecast(result) }
+        }
+    }
 
+    private fun bindForecast(forecast: Forecast) = with(forecast) {
+        Picasso.with(ctx).load(iconUrl).into(icon)
+        supportActionBar?.subtitle = date.toDateString(DateFormat.FULL)
+        weatherDescription.text = description
+        bindWeather(high to maxTemperature, low to minTemperature)
+    }
+
+    private fun bindWeather(vararg views: Pair<Int, TextView>) = views.forEach {
+        it.second.text = "${it.first}º"
+        it.second.textColor = color(when (it.first) {
+            in -50..0 -> android.R.color.holo_red_dark
+            in 0..15 -> android.R.color.holo_orange_dark
+            else -> android.R.color.holo_green_dark
+        })
     }
 }
